edb7809b704507d8e34fd659cd3408a024255eed
Zach Cossman <Zachcos@gmail.com>
2021-02-25 01:07:03 -0500

    Create stats card

--- src/components/content.js
+++ src/components/content.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { color } from '../imports/variables';
 
 import Card from './card';
 import companyLogo from '../../static/images/logo-mastercraft.svg';
@@ -38,6 +39,49 @@ const AboutCard = styled.div`
   }
 `;
 
+const StatsCard = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 50px 5%;
+  .stat {
+    border-right: 1px solid #d9d9d9;
+    color: $darkGray;
+    display: flex;
+    flex-direction: column;
+    width: 28%;
+    margin-left: 7%;
+    &:first-child {
+      margin-left: 0;
+    }
+    &:nth-child(3) {
+      border-right: none;
+    }
+    .feature {
+      color: $black;
+      font-size: 2rem;
+      font-weight: 700;
+      margin-bottom: 15px;
+    }
+  }
+  .progress {
+    width: 100%;
+    height: 12px;
+    background: #f4f4f4;
+    margin-top: 35px;
+    border-radius: 25px;
+    position: relative;
+    &__fill {
+      width: 76%;
+      height: 12px;
+      background: ${color.medCyan};
+      position: absolute;
+      left: 0;
+      top: 0;
+      margin-top: 0;
+    }
+  }
+`;
+
 export default function Content() {
   return (
     <ContentContainer>
@@ -64,6 +108,25 @@ export default function Content() {
             </div>
           </div>
         </AboutCard>
+      </Card>
+      <Card>
+        <StatsCard>
+          <div className="stat">
+            <div className="feature">$89,914</div>
+            of $100,000 backed
+          </div>
+          <div className="stat">
+            <div className="feature">5,007</div>
+            total backers
+          </div>
+          <div className="stat">
+            <div className="feature">56</div>
+            days left
+          </div>
+          <div className="progress">
+            <div className="progress progress__fill" />
+          </div>
+        </StatsCard>
       </Card>
     </ContentContainer>
   );
