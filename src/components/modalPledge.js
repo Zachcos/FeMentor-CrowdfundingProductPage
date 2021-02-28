be3c3c083d4370d3198551e76b314a8b33550f8c
Zach Cossman <Zachcos@gmail.com>
2021-02-28 12:08:03 -0500

    Create ModalPledge component to begin refactor

---
+++ src/components/modalPledge.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import { color } from '../imports/variables';
+
+export default function ModalPledge() {
+  return (
+    <div>
+      <div>this is the modal pledge component</div>
+    </div>
+  );
+}
