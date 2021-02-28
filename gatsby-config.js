27ad49411a6974c51c28a3dfee78ca92f77aeb4d
Zach Cossman <Zachcos@gmail.com>
2021-02-28 12:47:25 -0500

    Add gatsby-source-filesystem and gatsby-transformer-json to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,18 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    'gatsby-transformer-json',
     {
       resolve: 'gatsby-plugin-layout',
       options: {
         component: require.resolve('./src/components/layout.js'),
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/data`,
       },
     },
   ],
