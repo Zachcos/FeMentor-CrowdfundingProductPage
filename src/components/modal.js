e184850f85af5b4789a4bb412f3a51f82f5f81d7
Zach Cossman <Zachcos@gmail.com>
2021-02-28 20:31:28 -0500

    Add conditional to render success modal when isSubmitted state is true

--- src/components/modal.js
+++ src/components/modal.js
@@ -5,6 +5,7 @@ import { color } from '../imports/variables';
 import Card from './card';
 import ModalPledge from './modalPledge';
 import closeBtn from '../../static/images/icon-close-modal.svg';
+import success from '../../static/images/icon-check.svg';
 
 const ModalOverlay = styled.div`
   background: rgba(0, 0, 0, 0.5);
@@ -43,11 +44,46 @@ const ContentContainer = styled.div`
 `;
 
 const SuccessContainer = styled.div`
-  display: none;
+  margin: 0 auto;
+  top: 500px;
+  position: relative;
+  width: 550px;
+  .inside {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .icon__check {
+      margin: 40px 0;
+      height: 90px;
+      width: 90px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .title {
+      font-size: 1.4rem;
+      font-weight: 700;
+      margin-bottom: 20px;
+    }
+    .desc {
+      color: ${color.darkGray};
+      font-size: 0.95rem;
+      line-height: 1.7rem;
+      margin-bottom: 40px;
+      text-align: center;
+    }
+    .btn {
+      font-size: 0.9rem;
+      margin-bottom: 40px;
+      padding: 18px 30px;
+    }
+  }
 `;
 
 export default function Modal({ pledges }) {
   const [isSelected, setSelected] = useState('');
+  const [isSubmitted, setSubmitted] = useState(true);
 
   const handleClick = (id) => {
     setSelected(id);
@@ -55,31 +91,45 @@ export default function Modal({ pledges }) {
 
   return (
     <ModalOverlay>
-      <ContentContainer>
-        <Card>
-          <div className="closeBtn">
-            <img src={closeBtn} alt="" />
-          </div>
-          <div className="card__title">Back this project</div>
-          <div className="card__subtitle">
-            Want to support us in bringing Mastercraft Bamboo Monitor Riser out
-            in the world?
-          </div>
-          {pledges.edges.map((item) => (
-            <ModalPledge
-              data={item.node}
-              key={item.node.id}
-              onClick={() => handleClick(item.node.id)}
-              currentSelected={isSelected}
-            />
-          ))}
-        </Card>
-      </ContentContainer>
-      <SuccessContainer>
-        Thanks for your support! Your pledge brings us one step closer to
-        sharing Mastercraft Bamboo Monitor Riser worldwide. You will get an
-        email once our campaign is completed. Got it!
-      </SuccessContainer>
+      {!isSubmitted ? (
+        <ContentContainer>
+          <Card>
+            <div className="closeBtn">
+              <img src={closeBtn} alt="" />
+            </div>
+            <div className="card__title">Back this project</div>
+            <div className="card__subtitle">
+              Want to support us in bringing Mastercraft Bamboo Monitor Riser
+              out in the world?
+            </div>
+            {pledges.edges.map((item) => (
+              <ModalPledge
+                data={item.node}
+                key={item.node.id}
+                onClick={() => handleClick(item.node.id)}
+                currentSelected={isSelected}
+              />
+            ))}
+          </Card>
+        </ContentContainer>
+      ) : (
+        <SuccessContainer>
+          <Card>
+            <div className="inside">
+              <div className="icon__check">
+                <img src={success} alt="" />
+              </div>
+              <div className="title">Thanks for your support!</div>
+              <div className="desc">
+                Your pledge brings us one step closer to sharing Mastercraft
+                Bamboo Monitor Riser worldwide. You will get an email once our
+                campaign is completed.
+              </div>
+              <div className="btn btn__primary">Got it!</div>
+            </div>
+          </Card>
+        </SuccessContainer>
+      )}
     </ModalOverlay>
   );
 }
