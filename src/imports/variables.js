06b34d4f734c2caa68412b0d1cde52f3e3a15a35
Zach Cossman <Zachcos@gmail.com>
2021-03-04 16:24:11 -0500

    Add responsive breakpoint variable

--- src/imports/variables.js
+++ src/imports/variables.js
@@ -4,3 +4,11 @@ export const color = {
   black: 'hsl(0, 0%, 0%)',
   darkGray: 'hsl(0, 0%, 48%)',
 };
+
+const size = {
+  phone: '600px',
+};
+
+export const device = {
+  phone: `(max-width: ${size.phone})`,
+};
