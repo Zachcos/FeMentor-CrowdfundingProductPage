a5ace2f9a1e4d05dacc972b1fbfc162ec98a14fb
Zach Cossman <Zachcos@gmail.com>
2021-03-04 16:32:21 -0500

    Add initial hero styles for mobile

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import { device } from '../imports/variables';
 
 import heroBg from '../../static/images/image-hero-desktop.jpg';
+import heroBgMobile from '../../static/images/image-hero-mobile.jpg';
 import logo from '../../static/images/logo.svg';
 
 const HeroContainer = styled.div`
@@ -10,12 +12,39 @@ const HeroContainer = styled.div`
   background-size: cover;
   height: 400px;
   padding-top: 50px;
-  width: 100vw;
+  width: 100%;
+  @media screen and (${device}) {
+    background: linear-gradient(
+        to bottom,
+        rgba(0, 0, 0, 0.7) 0%,
+        transparent 25%
+      ),
+      url(${heroBgMobile}) no-repeat;
+    background-size: cover;
+    background-position: top center;
+    width: 100%;
+  }
 
   .topbar {
     display: flex;
     margin: 0 auto;
     width: 80%;
+    @media screen and (${device}) {
+      width: 90%;
+    }
+    .navBtn {
+      display: none;
+      @media screen and (${device}) {
+        display: inline-block;
+        height: 25px;
+        margin-left: auto;
+        width: 25px;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+    }
   }
 
   nav {
@@ -31,6 +60,9 @@ const HeroContainer = styled.div`
         cursor: pointer;
       }
     }
+    @media screen and (${device}) {
+      display: none;
+    }
   }
 `;
 
@@ -40,6 +72,9 @@ export default function Hero() {
       <div className="topbar">
         <div className="logo">
           <img src={logo} alt="" />
+        </div>
+        <div className="navBtn">
+          <img src="../../static/images/icon-hamburger.svg" alt="" />
         </div>
         <nav>
           <li>About</li>
